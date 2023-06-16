c331eb88eb53c8322305f69cb33ada8d04f8a9a6
dlakhno <dlakhno@gmail.com>
2023-06-16 14:17:14 +0600

    1. Add exception model; 2. Add handler tu handle exception; 3. Add example to handle exception

---
+++ src/main/java/kz/example/controller/ReactiveMathValidationController.java
@@ -0,0 +1,52 @@
+package kz.example.controller;
+
+import kz.example.dto.Response;
+import kz.example.exception.InputValidationException;
+import kz.example.service.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
+@RequiredArgsConstructor
+public class ReactiveMathValidationController {
+
+    private final ReactiveMathService reactiveMathService;
+
+    @GetMapping("square/{input}/throw")
+    public Mono<Response> findSquare(@PathVariable int input) {
+
+        if (input < 10 || input > 20) {
+            throw new InputValidationException(input);
+        }
+
+        return reactiveMathService.findSquare(input);
+    }
+
+    @GetMapping("square/{input}/mono-error")
+    public Mono<Response> monoError(@PathVariable int input) {
+        return Mono.just(input)
+                .handle((integer, sink) -> {
+                    if (integer >= 10 && integer <=20)
+                        sink.next(integer);
+                    else
+                        sink.error(new InputValidationException(integer));
+                })
+                .cast(Integer.class)
+                .flatMap(reactiveMathService::findSquare);
+    }
+
+    @GetMapping("square/{input}/assignment")
+    public Mono<ResponseEntity<Response>> assignment(@PathVariable int input) {
+        return Mono.just(input)
+                .filter(i -> i >= 10 && i <=20)
+                .flatMap(reactiveMathService::findSquare)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.badRequest().build());
+    }
+}
---
+++ src/main/java/kz/example/dto/InputFailedValidationResponse.java
@@ -0,0 +1,11 @@
+package kz.example.dto;
+
+import lombok.Data;
+
+@Data
+public class InputFailedValidationResponse {
+
+    private int errorCode;
+    private int input;
+    private String message;
+}
