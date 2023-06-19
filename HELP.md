f82bbeee1925f4da2190cdb3bb31a1fb29110af1
dlakhno <dlakhno@gmail.com>
2023-06-19 10:07:59 +0600

    1. Add handler to router; 2. fix readme

--- HELP.md
+++ HELP.md
@@ -10,4 +10,12 @@
 ### Reactive RESTs
 * GET : http://localhost:8080/reactive-math/square/5
 * GET : http://localhost:8080/reactive-math/table/5
+* GET : http://localhost:8080/reactive-math/table/{input}/stream
+* POST : http://localhost:8080/reactive-math/multiply Body: {"first": 5,"second": 5}
 
+
+### Functional RESTs
+* GET : http://localhost:8080/router/square/5
+* GET : http://localhost:8080/router/table/5
+* GET : http://localhost:8080/router/table/{input}/stream
+* POST : http://localhost:8080/router/multiply Body: {"first": 5,"second": 5}
---
+++ src/main/java/kz/example/config/RequestHandler.java
@@ -0,0 +1,45 @@
+package kz.example.config;
+
+import kz.example.dto.MultiplyRequestDto;
+import kz.example.dto.Response;
+import kz.example.service.ReactiveMathService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class RequestHandler {
+
+    private final ReactiveMathService mathService;
+
+    public Mono<ServerResponse> squareHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Mono<Response> responseMono = mathService.findSquare(input);
+        return ServerResponse.ok().body(responseMono, Response.class);
+    }
+
+    public Mono<ServerResponse> tableHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = mathService.multiplicationTable(input);
+        return ServerResponse.ok().body(responseFlux, Response.class);
+    }
+
+    public Mono<ServerResponse> tableStreamHandler(ServerRequest serverRequest) {
+        int input = Integer.parseInt(serverRequest.pathVariable("input"));
+        Flux<Response> responseFlux = mathService.multiplicationTable(input);
+        return ServerResponse.ok()
+                .contentType(MediaType.TEXT_EVENT_STREAM)
+                .body(responseFlux, Response.class);
+    }
+
+    public Mono<ServerResponse> multiplyHandler(ServerRequest serverRequest) {
+        Mono<MultiplyRequestDto> requestDtoMono = serverRequest.bodyToMono(MultiplyRequestDto.class);
+        Mono<Response> responseMono = mathService.multiply(requestDtoMono);
+        return ServerResponse.ok().body(responseMono, Response.class);
+    }
+}
