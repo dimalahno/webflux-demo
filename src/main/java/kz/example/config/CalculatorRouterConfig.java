97fb05d4a9e4c32f859bdb28a56a697de393d600
dlakhno <dlakhno@gmail.com>
2023-06-20 20:38:23 +0600

    add router in functional style

---
+++ src/main/java/kz/example/config/CalculatorRouterConfig.java
@@ -0,0 +1,34 @@
+package kz.example.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class CalculatorRouterConfig {
+
+    private final CalculatorHandler handler;
+
+    @Bean
+    public RouterFunction<ServerResponse> highLevelCalculatorRouter() {
+        return RouterFunctions.route()
+                .path("calculator", this::serverResponseRouterFunction)
+                .build();
+    }
+
+    private RouterFunction<ServerResponse> serverResponseRouterFunction() {
+        return RouterFunctions.route()
+                .GET("{a}/{b}", isOperation("+"), handler::additionalHandler)
+                .GET("{a}/{b}", isOperation("-"), handler::subtractionHandler)
+                .GET("{a}/{b}", isOperation("*"), handler::multiplicationHandler)
+                .GET("{a}/{b}", isOperation("/"), handler::divisionHandler)
+                .GET("{a}/{b}", request -> ServerResponse.badRequest().bodyValue("OP should be +, -, *, /"))
+                .build();
+    }
+
+    private RequestPredicate isOperation(String operation) {
+        return RequestPredicates.headers(headers -> operation.equals(headers.asHttpHeaders().toSingleValueMap().get("OP")));
+    }
+}
